00a8e942a8a863c4feaa11b09e5f85c5cbc6dde7
hinhtv <hinhtv@foobla.com>
2025-03-27 16:53:13 +0700

    gen-audio

--- src/action/gen-audio.js
+++ src/action/gen-audio.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const { slugify } = require('../helper/slugify');
 const Bluebird = require('bluebird');
-const { toTTS } = require('../helper/tts');
+const { toTTSAdvanced } = require('../helper/tts');
 const { mergeAudioFiles } = require('../helper/merge_mp3');
+const _ = require('lodash');
 
 // eslint-disable-next-line no-undef
 const audioDir = path.join(__dirname, '../../files/audio_files');
@@ -12,45 +13,76 @@ const libDir = path.join(__dirname, '../../files/lib');
 // eslint-disable-next-line no-undef
 const jsonDir = path.join(__dirname, '../../files/json_files');
 
-const handler = async (fileNames) => {
-    await Bluebird.mapSeries(fileNames, async fileName => {
-        const jsonFileName = `${slugify(fileName)}.json`;
-        const jsonFilePath = path.join(jsonDir, jsonFileName);
-        const data = require(jsonFilePath);
+const ensureDirectoryExists = (dirPath) => {
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true });
+    }
+};
 
-        await Bluebird.mapSeries(data, async item => {
-            const { english, vietnamese } = item
+const processAudioFiles = async (fileName, fileData) => {
+    const fileFilePath = path.join(audioDir, slugify(fileName));
+    ensureDirectoryExists(fileFilePath);
 
-            const mp3Files = await Promise.all([
-                toTTS(english, 'en'),
-                toTTS(vietnamese, 'vi'),
-            ]);
+    await Bluebird.mapSeries(fileData, async (item) => {
+        const { english, vietnamese } = item;
+        const mp3Files = await generateTTSFiles(english, vietnamese);
 
-            const mergedAudioFile = path.join(audioDir, `${slugify(english)}_merge.mp3`);
-
+        try {
+            const mergedAudioFile = path.join(fileFilePath, `${slugify(english)}_merge.mp3`);
             await mergeAudioFiles({
                 inputFiles: mp3Files,
                 outputFile: mergedAudioFile,
                 silenceFile: path.join(libDir, 'silence_3s.mp3')
             });
 
-
             if (!fs.existsSync(mergedAudioFile)) {
                 throw new Error(`Merged audio file does not exist: ${mergedAudioFile}`);
             }
-
-            mp3Files.forEach(file => {
-                if (fs.existsSync(file)) {
-                    fs.unlinkSync(file);
-                }
-            });
-
             console.log(`Merged audio file: ${mergedAudioFile}`);
-            throw new Error(`Merged audio file: ${mergedAudioFile}`);
-        })
-    })
+        } catch (error) {
+            console.log({ error: error.message });
+            throw error;
+        } finally {
+            cleanupFiles(mp3Files);
+        }
+    });
+};
 
-    console.log(`[Successfully]`)
-}
+const generateTTSFiles = async (english, vietnamese) => {
+    return Promise.all([
+        toTTSAdvanced({ text: english, lang: 'en' }),
+        toTTSAdvanced({ text: vietnamese, lang: 'vi' })
+    ]);
+};
 
-handler(['Do you want to'])
+const cleanupFiles = (files) => {
+    _.forEach(files, (file) => {
+        if (fs.existsSync(file)) {
+            fs.unlinkSync(file);
+        }
+    });
+};
+
+const handler = async (fileNames) => {
+    await Bluebird.mapSeries(fileNames, async (fileName) => {
+        const slugifyFileName = `${slugify(fileName)}.json`;
+        const jsonFilePath = path.join(jsonDir, slugifyFileName);
+
+        if (!fs.existsSync(jsonFilePath)) {
+            console.log(`JSON file not found: ${jsonFilePath}`);
+            return;
+        }
+
+        const fileData = require(jsonFilePath);
+        if (!Array.isArray(fileData) || !fileData.length) {
+            console.log(`Error processing file: ${fileName} (invalid data)`);
+            return;
+        }
+
+        await processAudioFiles(fileName, fileData);
+    });
+
+    console.log(`[Successfully]`);
+};
+
+handler(['Do you want to']);
--- src/helper/tts.js
+++ src/helper/tts.js
@@ -27,3 +27,26 @@ exports.toTTS = async (text, lang = 'en') => {
         throw error;
     }
 };
+
+exports.toTTSAdvanced = async ({ text, lang = 'en' }) => {
+    try {
+        const slugText = slugify(text);
+        const uniqueFileName = `${slugText}.mp3`;
+        const filePath = path.join('files/draft_files', uniqueFileName);
+
+        const speech = new gTTS(text, lang);
+
+        await new Promise((resolve, reject) => {
+            speech.save(filePath, (err) => {
+                if (err) reject(err);
+                else resolve();
+            });
+        });
+
+        console.log('Đã tạo file âm thanh thành công:', filePath);
+        return filePath;
+    } catch (error) {
+        console.error('Có lỗi xảy ra:', error);
+        throw error;
+    }
+}
